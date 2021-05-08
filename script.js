28bea1a3e5cd54583dda41049ffd4599417a70e2
Jacob Cohn <jacobnc916@gmail.com>
2021-05-08 06:41:32 -0500

    finished all basic features

--- script.js
+++ script.js
@@ -1,52 +1,102 @@
-const LOTF = {
-    title: 1,
-    author: 2,
-    pages: 3,
-    read: 4,
-}
-const GOT = {
-    title: 5,
-    author: 6,
-    pages: 7,
-    read: 8,
-}
-let myLibrary = [LOTF, GOT];
+let myLibrary = [];
+let readBtns, removeBtns;
 
 // book constructor
-function Book(title, author, pages, read){
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
 }
+
+const addBtn = document.querySelector('#addBtn');
+addBtn.addEventListener('click', () => addBookToLibrary());
 
 function addBookToLibrary() {
     // create the book object from user input
-    const userInput = new Book(prompt('Title?'), prompt('Author?'), prompt('Pages?'), prompt('Read?'));
+    const userInput = new Book(prompt('Title?'), prompt('Author?'), prompt('Pages?'));
     // add book object to myLibrary and display it
     myLibrary.push(userInput);
+    updateLibrary();
+}
+
+function updateLibrary() {
+    // updates position
+    myLibrary.forEach(item => {
+        const containsItem = element => element == item;
+        const bookPosition = myLibrary.findIndex(containsItem);
+        item.position = bookPosition;
+    })
+    // updates display
+    deleteCurrentDisplay();
     displayMyLibrary();
+    // updates readBtns and removeBtns
+    readBtns = Array.from(document.querySelectorAll('.readBtn'));
+    removeBtns = Array.from(document.querySelectorAll('.removeBtn'));
+    addEventsToBtns();
+}
+
+
+const booksGrid = document.querySelector('#booksGrid');
+
+function deleteCurrentDisplay() {
+    while (booksGrid.firstChild) {
+        booksGrid.removeChild(booksGrid.firstChild);
+    }
 }
 
 function displayMyLibrary() {
-    const booksGrid = document.querySelector('#booksGrid');
-    // (MAYBE) deletes all current DOM
     // loop that goes through all objects in array
     myLibrary.forEach(item => {
         // creates book card for each book
         const bookCard = document.createElement('div')
         booksGrid.appendChild(bookCard);
-        bookCard.id = item.title + ' Book Card';
+        bookCard.id = item.position + ' Book Card';
         bookCard.classList.toggle('bookCard');
         // creates div element for each property of each book
         for (let key in item) {
-            const bookCardProperties = document.createElement('div');
-            bookCard.appendChild(bookCardProperties);
-            bookCardProperties.innerHTML = item[key];
-            bookCardProperties.id = item.title + ' ' + key;
-            bookCardProperties.classList.toggle(key);
+            if (key !== 'position') {
+                const bookCardProperties = document.createElement('div');
+                bookCard.appendChild(bookCardProperties);
+                bookCardProperties.innerHTML = key + ': ' + item[key];
+                bookCardProperties.id = item.position + ' ' + key;
+                bookCardProperties.classList.toggle(key);
+            }
         }
-    });  
+        // creating read button
+        const readBtn = document.createElement('button');
+        bookCard.appendChild(readBtn);
+        readBtn.innerHTML = 'Read';
+        readBtn.id = item.position + ' readBtn';
+        readBtn.classList.toggle('readBtn');
+        readBtn.classList.toggle('read');
+        // creating remove button
+        const removeBtn = document.createElement('button');
+        bookCard.appendChild(removeBtn);
+        removeBtn.innerHTML = 'Remove';
+        removeBtn.id = item.position + ' removeBtn';
+        removeBtn.classList.toggle('removeBtn');
+    }) 
 }
 
-displayMyLibrary();
+function addEventsToBtns() {
+    // switches between 'Read' and 'Not Read' when readBtn clicked
+    readBtns.forEach(item => {
+        item.addEventListener('click', e => {
+            if (e.target.classList.contains('read')) {
+                item.innerHTML = 'Not Read';
+            } else {
+                item.innerHTML = 'Read';
+            };
+            item.classList.toggle('read');
+            item.classList.toggle('not-read');
+        })
+    })
+    // remove book card when removeBtn clicked
+    removeBtns.forEach(item => {
+        item.addEventListener('click', e => {
+            const bookCardPosition = e.target.id.split(' ')[0];
+            myLibrary.splice(bookCardPosition, 1);
+            updateLibrary();
+        })
+    })
+}
